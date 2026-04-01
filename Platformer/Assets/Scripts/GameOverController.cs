aeec55fc95900a5cd6295e7eacf7eb37f02f20ad
Aaron <amccurry209@gmail.com>
2026-04-01 11:45:34 -0400

    Added sqlite database for high score tracking and added leaderboard scene

--- Platformer/Assets/Scripts/GameOverController.cs
+++ Platformer/Assets/Scripts/GameOverController.cs
@@ -5,11 +5,29 @@ using UnityEngine.SceneManagement;
 public class GameOverController : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public TMP_InputField playerNameInput;
 
     void Start()
     {
         int finalScore = GameManager.Instance.finalScore;
-        scoreText.text = "Final Score: " + finalScore;
+        scoreText.text = "Final Score: " + finalScore;        
+    }
+
+    public void OnSubmitScore()
+    {
+        string playerName = playerNameInput.text;
+        
+        if (string.IsNullOrEmpty(playerName))
+        {
+            playerName = "Anonymous";
+        }
+        
+        int finalScore = GameManager.Instance.finalScore;
+        float completionTime = Time.timeSinceLevelLoad;
+        
+        DatabaseManager.Instance.SaveHighScore(playerName, finalScore, completionTime);
+        
+        SceneManager.LoadScene("HighScores");
     }
 
     public void Retry()
